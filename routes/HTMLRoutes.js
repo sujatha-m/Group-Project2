9a4106bf2dd7b00efbe9c8133f522c375beacab4
Brian D <dinhlol69@gmail.com>
2020-05-26 20:32:41 -0400

    Convert the home page to now use handlebars

--- routes/HTMLRoutes.js
+++ routes/HTMLRoutes.js
@@ -6,10 +6,33 @@ const isAuthenticated = require('../config/middleware/isAuthenticated')
 
 const PUBLIC_FOLDER_PATH = path.join(process.cwd(), 'public')
 
+// This is use to inject library into the HTML document head.
+// I couldn't find a better way of doing this as Express render method doesn't make it easy to do.
+//
+// To inject library into a page, pass a object with the title, css array, and script array into Express render method option.
+const lib = {
+  global: 'style/global.css',
+  bulma: 'vendor/bulma.min.css',
+  fonts: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+  fontawesome: 'https://use.fontawesome.com/releases/v5.3.1/js/all.js'
+}
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(PUBLIC_FOLDER_PATH, 'index.html'))
+  res.render('index', {
+    title: 'Spam Numbers Tracker',
+    css: [
+      lib.bulma,
+      lib.global,
+      lib.fonts,
+      'style/home.css'
+    ],
+    script: [
+      lib.fontawesome,
+      'js/home.js'
+    ]
+  })
 })
 
 router.get('/login', (req, res) => {
